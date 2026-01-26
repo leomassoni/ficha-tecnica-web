b28b07a8be8ea5f2f01ca525c24d771a1ecf19e7
leomassoni <leonardo_massoni@hotmail.com>
2026-01-26 01:38:11 -0300

    Resume <p> do <header> para uma versão menos extensa

--- src/App.tsx
+++ src/App.tsx
@@ -195,7 +195,7 @@ export default function App() {
     <div className="page">
       <header className="header">
         <h1>Ficha Técnica</h1>
-        <p>Selecione uma receita e ajuste a volumetria. O app recalcula automaticamente.</p>
+        <p>Selecione uma receita e defina o volume ou a quantidade de receitas a ser produzido.</p>
       </header>
 
       <section className="controls">
